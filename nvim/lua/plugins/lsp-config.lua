695aea4fc3beec57c206383ef6ce6c2b10ef9578
Asharib Ansari <heyyshaarib@gmail.com>
2025-06-24 20:43:50 +0000

    merge mason.lua and lsp.lua

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,40 @@
+return {
+    {
+        "mason-org/mason.nvim",
+        config = function()
+            require("mason").setup({
+            })
+        end
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "pyright",
+                    "ruff",
+                    "clangd",
+                },
+            })
+        end
+    }, 
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.pyright.setup({})
+            lspconfig.ruff.setup({})
+            lspconfig.clangd.setup({})
+
+            -- key_bindings
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+        end
+    },
+}
