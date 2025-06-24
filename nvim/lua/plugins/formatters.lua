52be1afc773b287b2c3e336602284ceb31c195fe
Asharib Ansari <heyyshaarib@gmail.com>
2025-06-24 21:50:48 +0000

    add ruff

--- nvim/lua/plugins/formatters.lua
+++ nvim/lua/plugins/formatters.lua
@@ -1,18 +1,37 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        dependencies = { "mason.nvim" },
-        lazy = true,
-        cmd = "ConformInfo",
-        keys = {
-          {
-            "<leader>cF",
-            function()
-              require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
-            end,
-            mode = { "n", "v" },
-            desc = "Format Injected Langs",
-          },
+  {
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
+    lazy = true,
+    cmd = "ConformInfo",
+    keys = {
+      {
+        "<leader>cf",
+        function()
+          require("conform").format({ timeout_ms = 3000 })
+        end,
+        mode = { "n", "v" },
+        desc = "Format File",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_format" }, -- use ruff as formatter
+        c = { "clang_format" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+      },
+      formatters = {
+        ruff_format = {
+          command = vim.fn.stdpath("data") .. "/mason/bin/ruff",
+          args = { "format", "-" },
+          stdin = true,
         },
-    }
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        timeout_ms = 3000,
+      },
+    },
+  },
 }
