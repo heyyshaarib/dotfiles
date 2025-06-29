e08b552ae2321419905544a98c3dce1cd1bbb382
Asharib Ansari <heyyshaarib@gmail.com>
2025-06-29 16:45:47 +0000

    enabled notifier

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -4,11 +4,8 @@ return {
         priority = 1000,
         lazy = false,
         opts = {
-          -- your configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
           bigfile = { enabled = true },
-          -- notifier = { enabled = true },
+          notifier = { enabled = true },
           quickfile = { enabled = true },
           statuscolumn = { enabled = true },
           words = { enabled = true },
